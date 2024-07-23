362532472f931212167b85173e9bf43990045b41
Alexandre Celles <alexandre.celles@edu.ece.fr>
2024-07-23 17:27:02 +0200

    Test

--- annulerRDV.php
+++ annulerRDV.php
@@ -16,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['annuler']))
 
     // Supprimer la réservation
     $sql = "DELETE FROM Reservation WHERE ID_reservation = $ID_reservation";
-    $sql2 = "UPDATE Disponibilite d JOIN Reservation r ON d.Jour = r.jour AND d.Heure_Debut = r.heure SET d.Statut = 'Disponible' WHERE r.reservation_id = $ID_reservation";
+    $sql2 = "UPDATE Disponibilite d JOIN Reservation r ON d.Jour = r.jour AND d.Heure_Debut = r.heure SET d.Statut = 'Disponible' WHERE r.ID_reservation = $ID_reservation";
     $conn->query($sql2);
     if($conn->query($sql))
     {
